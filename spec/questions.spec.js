33b6883947075e933c5fce82a30c4c0c0158757f
Wiam Malek <wiam.malek95@gmail.com>
2021-06-02 16:48:52 +0200

    spec.js and questions.js are modified

--- spec/questions.spec.js
+++ spec/questions.spec.js
@@ -2,47 +2,64 @@ describe('Les chaines de caractères : ', function () {//on décrit la fonctionn
   // "it" indique le comportement attendu
     it('Donner la taille d\'une chaine de caractères', function () {
       // on crée un exemple
-        var result = tailleString('Ce texte à une certaine taille');
+        var result = tailleString('Emma');
         // on donne le reésultat attendu
-        expect(result).toEqual(30);
+        expect(result).toEqual(4);
     });
 
-    // it('Remplacer le premier e d\'une chaine par un espace', function () {
+    it('Remplacer le premier e d\'une chaine par un espace', function () {
+        var resultSpace = spaceString('je suis Emma et j ai 34ans');
+        expect(resultSpace).toMatch('j  suis Emma et j ai 34ans')
+
+    });
+
+      it('Concatener deux chaines de caractères', function () {
+          var result = concatenate('Salut', 'Emma');
+          expect(result).toEqual('Salut Emma');
+      });
     //
-    // });
+      it('Afficher le cinquième caractère d\'une chaine', function () {
+          let character = fifthCharacter('je suis sur la quatriémme exo') ;
+          expect(character).toBe('u');
+      });
     //
-    //   it('Concatener deux chaines de caractères', function () {
+    it('Afficher les 9 premiers caractères d\'une chaîne de caractère', function () {
+        var stringCharacter = displayNinthCharacter('je suis sur la cinquieme exo');
+        expect(stringCharacter).toBe('je suis s')
+    });
     //
-    //   });
+    it('Mettre en majuscule la chaine', function () {
+        var stringUpperCase = capitalize('Le sixieme exo');
+        expect(stringUpperCase).toBe('LE SIXIEME EXO');
+    });
     //
-    // it('Afficher le cinquième caractère d\'une chaine', function () {
-    //
-    // });
-    //
-    // it('Afficher les 9 premiers caractères d\une chaîne de caractère', function () {
-    //
-    // });
-    //
-    // it('Mettre en majuscule la chaine', function () {
-    //
-    // });
-    //
-    // it('Mettre en minuscule la chaine', function () {
-    //
-    // });
-    // it('Supprimer les espaces avant et après la chaine', function () {
-    //
-    // });
-    // it('Afficher true si le parametre d\'entrée de la fonction est de type string', function () {
-    //
-    // });
-    // it('Afficher l\'extension du fichier', function () {
-    //
-    // });
-    // it('Compter le nombre d\'espace dans la chaine', function () {
-    //
-    // });
-    // it('Inverser une chaine de caractères', function () {
-    //
-    // });
+    it('Mettre en minuscule la chaine', function () {
+      var stringLowerCase = lowerCaseString('Le septième exercice');
+      expect(stringLowerCase).toBe('le septième exercice');
+    });
+
+    it('Supprimer les espaces avant et après la chaine', function () {
+       var removeWhiteSpace = whiteSpace(' le dixiéme exo ');
+       expect(removeWhiteSpace).toEqual('le dixiéme exo') 
+    });
+
+    it('Afficher true si le parametre d\'entrée de la fonction est de type string', function () {
+        var enterParameter = functionParameter('parameter');
+        // expect(enterParameter).toEqual(true)
+        expect(enterParameter).toBeTrue();
+    });
+    it('Afficher l\'extension du fichier', function () {
+        var displayFileExtention = fileExtention ('index.js');
+        expect(displayFileExtention).toEqual('js')
+    });
+
+    it('Compter le nombre d\'espace dans la chaine', function () {
+        var compterNombreEspace = compterEspace('le avant dernier exercice')
+        expect(compterNombreEspace).toEqual(3);
+    });
+    it('Inverser une chaine de caractères', function () {
+         var stringReverse = reverseString('wiam')
+         expect(stringReverse).toEqual('maiw')
+    });
 })
+
--- src/questions.js
+++ src/questions.js
@@ -3,3 +3,68 @@
  * Trouvez la façon de faire la plus optimal.
  * Il peut y avoir plusieur façon de faire.
  */
+var tailleString = function(text){
+    return text.length;
+}
+//Concatener deux chaines de caractères
+// var concatenate = function(){
+//     let a  = "Salut";
+//     let b = "Emma";
+//     return resultAttendu = a + " " + b;
+// }
+
+var concatenate = function(a, b){
+  
+    return a + " " + b;
+}
+//Remplacer le premier e d\'une chaine par un espace
+var spaceString = function(replaceE){
+    return replaceE.replace('e', ' ');
+}
+
+//Afficher le cinquième caractère d\'une chaine
+var fifthCharacter = function(stringCharacter){
+    return stringCharacter.charAt(4);
+}
+
+//Afficher les 9 premiers caractères d\'une chaîne de caractère
+var displayNinthCharacter = function(x){
+    return x.substr(0, 9);
+}
+
+//Mettre en majuscule la chaine
+var capitalize = function(upperString){
+    return upperString.toUpperCase();
+}
+//Mettre en minuscule la chaine
+var lowerCaseString = function(lowerString){
+    return lowerString.toLowerCase();
+}
+//Supprimer les espaces avant et après la chaine
+var whiteSpace = function(space){
+    return space.trim();
+}
+//Afficher true si le parametre d\'entrée de la fonction est de type string
+var functionParameter = function(string){
+    if( typeof string === 'string'){
+        return true;
+    }
+}
+//Afficher l\'extension du fichier
+var fileExtention = function(file){
+    return file.split('.').pop();
+}
+//Compter le nombre d\'espace dans la chaine
+var compterEspace = function(space){
+    return space.split(' ').length -1
+}
+//Inverser une chaine de caractères
+var reverseString = function(string){
+    return string.split('').reverse().join('');
+}
+
+
+
+// split(): cette méthode divisera la chaîne en un tableau de caractères.
+// reverse(): cette méthode inverse tous les éléments d’un tableau.
+// join(): Cette méthode joint tous les éléments du tableau pour créer une chaîne de caractères.
